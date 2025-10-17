2c2025c9bf8fca69142b0ee45925d1b1093343ae
kojamad28 <kojamad28@gmail.com>
2025-10-18 07:11:54 +0900

    updated dependencies

--- install_packages.R
+++ install_packages.R
@@ -1,9 +1,22 @@
 cran_packages <- c(
-    "tidyverse", "anndata", "Seurat", "Signac", "WGCNA"
+  "tidyverse",
+  "anndata",
+  "Seurat",
+  "Signac",
+  "WGCNA",
+  "loadings",
+  "mseapca"
 )
 install.packages(cran_packages, dependencies = TRUE)
 
 bioconductor_packages <- c(
-    "BiocVersion", "BioBase", "BiocGenerics", "DESeq2", "edgeR", "monocle", "clusterProfiler", "mixOmics"
+  "BiocVersion",
+  "BioBase",
+  "BiocGenerics",
+  "DESeq2",
+  "edgeR",
+  "monocle",
+  "clusterProfiler",
+  "mixOmics"
 )
 BiocManager::install(bioconductor_packages, dependencies = TRUE)
